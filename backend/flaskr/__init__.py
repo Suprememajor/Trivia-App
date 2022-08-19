51e651b27a64fc25f15d62e556af157b7b7ed12a
Suprememajor <ndipnobertetta@gmail.com>
2022-08-19 20:18:11 +0100

    finished category tests

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -42,41 +42,55 @@ def create_app(test_config=None):
 
     """
 
-    @app.route("/categories", methods=["POST"])
-    def create_category():
-        body = request.get_json()
-        name = body.get("name", None)
-        if not name:
-            abort(400)
-        name = name.title()
-        if Category.query.filter_by(name=name).first():
-            abort(409)
-        category = Category(name=name)
-        try:
-            category.insert()
-            categories = [category.format() for category in Category.query.all()]
+    # @app.route("/categories", methods=["POST"])
+    # def create_category():
+    #     body = request.get_json()
+    #     name = body.get("name", None)
+    #     if not name:
+    #         abort(400)
+    #     name = name.title()
+    #     if Category.query.filter_by(name=name).one_or_none():
+    #         abort(409)
+    #     category = Category(name=name)
+    #     try:
+    #         category.insert()
+    #
+    #         return jsonify(
+    #             {
+    #                 "success": True,
+    #                 "created": category.id
+    #             }
+    #         )
+    #
+    #     except:
+    #         abort(422)
 
-            return jsonify(
-                {
-                    "success": True,
-                    "created": category.id,
-                    "categories": categories
-                }
-            )
+    # @app.route("/categories")
+    # def retrieve_categories():
+    #     categories = Category.query.order_by(Category.name).all()
+    #     if len(categories) == 0:
+    #         abort(404)
+    #     categories = {cat.id: cat.name for cat in categories}
+    #     return jsonify({
+    #         "success": True,
+    #         "categories": categories
+    #     })
 
-        except Exception as e:
-            abort(422)
-
-    @app.route("/categories")
-    def retrieve_categories():
-        categories = Category.query.order_by(Category.name).all()
-        if len(categories) == 0:
-            abort(404)
-        categories = {cat.id: cat.name for cat in categories}
-        return jsonify({
-            "success": True,
-            "categories": categories
-        })
+    # @app.route("/categories/<int:category_id>/questions")
+    # def retrieve_category_questions(category_id):
+    #     category = Category.query.filter_by(id=category_id).one_or_none()
+    #     if not category:
+    #         abort(404)
+    #     selection = Question.query.filter_by(category_id=category_id).all()
+    #     current_questions = paginate_questions(request, selection)
+    #     if len(current_questions) == 0:
+    #         abort(404)
+    #     return jsonify({
+    #         "success": True,
+    #         "questions": current_questions,
+    #         "totalQuestions": len(selection),
+    #         "currentCategory": category.name
+    #     })
 
     # """
     # User
@@ -131,75 +145,90 @@ def create_app(test_config=None):
         new_answer = body.get("answer", None)
         new_category = body.get("category", None)
         new_difficulty = body.get("difficulty", None)
+        search_term = body.get("searchTerm", None)
+        cur_cat_id = request.args.get("category", None, type=int)
+        if cur_cat_id:
+            current_category = Category.query.get(cur_cat_id).one_or_none()
+            if current_category is None:
+                abort(404)
+        else:
+            current_category = Category(name="All")
 
-        if not (new_question and new_answer and new_category and new_difficulty):
+        if search_term:
+            selection = Question.query.filter(Question.question.ilike(f"%{search_term}%").all())
+            if len(selection) == 0:
+                abort(404)
+            current_questions = paginate_questions(request, selection)
+            return jsonify({
+                "success": True,
+                "questions": [question.format() for question in current_questions],
+                "totalQuestions": len(selection),
+                "currentCategory": current_category.name
+            })
+        elif new_question and new_answer and new_category and new_difficulty:
+            try:
+                question = Question(question=new_question, answer=new_answer, category_id=new_category,
+                                    difficulty=new_difficulty)
+                question.insert()
+                return jsonify({"success": True})
+            except:
+                abort(422)
+        else:
             abort(400)
 
-        try:
-            cat_all_id = Category.query.filter_by(name="All").first().id
-            cur_cat_id = request.args.get("category", cat_all_id, type=int)
-            question = Question(question=new_question, answer=new_answer, category_id=new_category,
-                                difficulty=new_difficulty)
-            question.insert()
-            selection = Question.query.filter_by(category_id=cur_cat_id).order_by(Question.id).all()
-            current_questions = paginate_questions(request, selection)
-            cats = Category.query.all()
-            current_category = Category.query.get(cur_cat_id).format()
-            return jsonify(
-                {
-                    "success": True,
-                    "current_category": current_category,
-                    "created": question.id,
-                    "categories": [cat.format() for cat in cats],
-                    "questions": current_questions,
-                    "total_questions": len(Question.query.all())
-                }
-            )
-
-        except Exception as e:
-            abort(422)
+    @app.route("/quizzes", methods=["POST"])
+    def get_quiz():
+        body = request.get_json()
+        previous_questions = body.get("previous_questions", None)
+        quiz_category = body.get("quiz_category", None)
+        if previous_questions and quiz_category:
+            question = Question.query.filter(Question.id.not_in(previous_questions)).first()
+            if question is None:
+                abort(404)
+            return jsonify({
+                "success": True,
+                "question": question.format()
+            })
+        else:
+            abort(400)
 
     @app.route("/questions")
     def retrieve_questions():
-        cat_all_id = Category.query.filter_by(name="All").first().id
-        cur_cat_id = request.args.get("category", cat_all_id, type=int)
-        selection = Question.query.filter_by(category_id=cur_cat_id).order_by(Question.id).all()
+        cur_cat_id = request.args.get("category", None, type=int)
+        if cur_cat_id:
+            current_category = Category.query.get(cur_cat_id).one_or_none()
+            if current_category is None:
+                abort(404)
+        else:
+            current_category = Category(name="All")
+        selection = Question.query.order_by(Question.id).all()
         current_questions = paginate_questions(request, selection)
-        categories = Category.query.all()
         if len(current_questions) == 0:
             abort(404)
-        cats = [cat.format() for cat in categories]
-        current_category = Category.query.get(cur_cat_id).format()
 
         return jsonify(
             {
                 "success": True,
-                "current_category": current_category,
-                "categories": cats,
+                "currentCategory": current_category.name,
+                "categories": {cat.id: cat.name for cat in Category.query.all()},
                 "questions": current_questions,
-                "total_questions": len(Question.query.all())
+                "totalQuestions": len(selection)
             }
         )
 
     @app.route("/questions/<int:question_id>", methods=["DELETE"])
     def delete_question(question_id):
         try:
-            cat_all_id = Category.query.filter_by(name="All").first().id
-            cur_cat_id = request.args.get("category", cat_all_id, type=int)
             question = Question.query.filter(Question.id == question_id).one_or_none()
             if question is None:
                 abort(404)
 
             question.delete()
-            selection = Question.query.filter_by(category_id=cur_cat_id).order_by(Question.id).all()
-            current_questions = paginate_questions(request, selection)
 
             return jsonify(
                 {
                     "success": True,
-                    "deleted": question_id,
-                    "questions": current_questions,
-                    "total_questions": len(Question.query.all())
+                    "deleted": question_id
                 }
             )
 
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -4,6 +4,7 @@ import unittest
 from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
+from models import Category
 
 
 class CategoryModelTestCase(unittest.TestCase):
@@ -16,7 +17,7 @@ class CategoryModelTestCase(unittest.TestCase):
         self.client = self.app.test_client
         self.app.config["SQLALCHEMY_DATABASE_URI"] = database_path
         self.app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-        self.new_category = {"name": "new category"}
+        self.new_category = {"name": "new category4"}
         self.new_category2 = {"name": "miSc"}
         self.new_category3 = {"body": "miSc"}
 
@@ -31,34 +32,33 @@ class CategoryModelTestCase(unittest.TestCase):
     def tearDown(self):
         """Executed after reach test"""
         pass
-#
-#     def test_create_new_category(self):
-#
-#         res = self.client().post("/categories", json=self.new_category)
-#         data = json.loads(res.data)
-#
-#         self.assertEqual(res.status_code, 200)
-#         self.assertEqual(data["success"], True)
-#         self.assertTrue(data["created"])
-#         self.assertTrue(len(data["categories"]))
-#
-#     def test_400_create_category_failed(self):
-#
-#         res = self.client().post("/categories", json=self.new_category3)
-#         data = json.loads(res.data)
-#
-#         self.assertEqual(res.status_code, 400)
-#         self.assertEqual(data["success"], False)
-#         self.assertEqual(data["message"], "bad request")
-#
-#     def test_409_create_category_failed(self):
-#
-#         res = self.client().post("/categories", json=self.new_category2)
-#         data = json.loads(res.data)
-#
-#         self.assertEqual(res.status_code, 409)
-#         self.assertEqual(data["success"], False)
-#         self.assertEqual(data["message"], "resource exists")
+
+    def test_create_new_category(self):
+
+        res = self.client().post("/categories", json=self.new_category)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["created"])
+
+    def test_400_create_category_failed(self):
+
+        res = self.client().post("/categories", json=self.new_category3)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "bad request")
+
+    def test_409_duplicate_category(self):
+
+        res = self.client().post("/categories", json=self.new_category2)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 409)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "resource exists")
 
     def test_get_categories(self):
         res = self.client().get("/categories")
@@ -67,29 +67,36 @@ class CategoryModelTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data["success"], True)
         self.assertTrue(len(data["categories"]))
-#     #
-#     # def test_delete_category(self):
-#     #     category_id = 6
-#     #     res = self.client().delete(f"/categories/{category_id}")
-#     #     data = json.loads(res.data)
-#     #     category = Category.query.filter(Category.id == category_id).one_or_none()
-#     #     questions = Question.query.filter(Question.category_id == category_id).all()
-#     #
-#     #     self.assertEqual(res.status_code, 200)
-#     #     self.assertEqual(data["success"], True)
-#     #     self.assertEqual(data["deleted"], category_id)
-#     #     self.assertTrue(len(data["categories"]))
-#     #     self.assertEqual(category, None)
-#     #     self.assertEqual(len(questions), 0)
-#     #
-#     # def test_422_if_category_does_not_exist(self):
-#     #
-#     #     res = self.client().delete("/categories/1000")
-#     #     data = json.loads(res.data)
-#     #
-#     #     self.assertEqual(res.status_code, 422)
-#     #     self.assertEqual(data["success"], False)
-#     #     self.assertEqual(data["message"], "unprocessable"
+
+    def test_get_cat_questions(self):
+        category_id = 6
+        category = Category.query.get(category_id)
+        res = self.client().get(f"/categories/{category_id}/questions")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertEqual(data["currentCategory"], category.name)
+        self.assertTrue(len(data["questions"]))
+        self.assertTrue(data["totalQuestions"])
+
+    def test_404_category_not_found(self):
+        category_id = 100
+        res = self.client().get(f"/categories/{category_id}/questions")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "resource not found")
+
+    def test_404_empty_category(self):
+        category_id = 8
+        res = self.client().get(f"/categories/{category_id}/questions")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "resource not found")
 
 
 class QuestionModelTestCase(unittest.TestCase):
@@ -140,16 +147,17 @@ class QuestionModelTestCase(unittest.TestCase):
     #     self.assertEqual(data["success"], False)
     #     self.assertEqual(data["message"], "bad request")
 
-    # def test_get_paginated_questions(self):
-    #     res = self.client().get(f"/questions")
-    #     data = json.loads(res.data)
-    #
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertEqual(data["success"], True)
-    #     self.assertTrue(data["total_questions"])
-    #     self.assertTrue(len(data["categories"]))
-    #     self.assertTrue(len(data["questions"]))
-    #
+    def test_get_paginated_questions(self):
+        page = 1
+        res = self.client().get(f"/questions?page={page}")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["total_questions"])
+        self.assertTrue(len(data["categories"]))
+        self.assertTrue(len(data["questions"]))
+
     # def test_get_paginated_questions_with_cat(self):
     #     category = 4
     #     res = self.client().get(f"/questions?category={category}")
